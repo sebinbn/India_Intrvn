823b416a1df99795c7ca89d4251f2ad2f0df83a5
unknown <sebinbn@gmail.com>
2024-12-15 18:03:39 -0600

    Slope calculated.

--- 1_YieldImport.R
+++ 1_YieldImport.R
@@ -1,14 +1,17 @@
-# This imports data downloaded from Bloomberg
+# This imports data downloaded from Bloomberg. It then calculates slopes. 
 
 ####Data import and cleaning####
 
 BBYield = read_xlsx("Bloomberg data/INDYC_3mo30y.xlsx", range = 'A3:T1657')      # Data available for some variables upto 1998. But, importing data from 2018 Jan
-#BBYield$Date = as.Date.POSIXct(BBYield[,1], format = "%Y-%m-%d") #unbale to convert this posixct
-#as.Date(as.character(BBYield[1,1]))
-BBYield[,-1] = lapply(BBYield[,-1], as.numeric)
 colnames(BBYield)[1] = "Date"
 colnames(BBYield)[-1] = c(substr(colnames(BBYield)[2:11],1,6),
                           substr(colnames(BBYield)[12:ncol(BBYield)],1,7) )     #renaming with shorter names
-                                        
+BBYield$Date = as.Date(BBYield$Date, format = "%Y-%m-%d") 
+BBYield[,-1] = lapply(BBYield[,-1], as.numeric)
+
 BBYield = BBYield[order(BBYield$Date),]
-str(BBYield) #exploring the structure of the data
+
+#Calculating empirical slope
+BBYield$s102 = BBYield$GIND10Y - BBYield$GIND2Y
+BBYield$s101 = BBYield$GIND10Y - BBYield$GIND1Y
+BBYield$s103 = BBYield$GIND10Y - BBYield$GIND3M
