c324effbd86d702f21c2068db2b67bb9425343e1
unknown <sebinbn@gmail.com>
2024-12-18 08:59:13 -0600

    Std. Devn title added.

--- 2_SummaryStats.R
+++ 2_SummaryStats.R
@@ -6,9 +6,17 @@ print("Pre-Intervention Summary")
 print(summary(Merge_dat[Merge_dat$Date >= as.Date("2018-04-01") & 
                     Merge_dat$Date <= as.Date("2019-11-30"),
                   !colnames(Merge_dat) %in% c("Date","D_Ann","D_Auc")]))
+print("Std.Devn")
+print(apply(Merge_dat[Merge_dat$Date >= as.Date("2018-04-01") & 
+            Merge_dat$Date <= as.Date("2019-11-30"),
+          !colnames(Merge_dat) %in% c("Date","D_Ann","D_Auc")], 2, sd))
 
 # Intervention summary -------------------------------------------------
 print("Intervention Summary")
 print(summary(Merge_dat[Merge_dat$Date >= as.Date("2019-12-01") & 
                     Merge_dat$Date <= as.Date("2021-05-31"),
                   !colnames(Merge_dat) %in% c("Date","D_Ann","D_Auc")]))
+print("Std.Devn")
+print(apply(Merge_dat[Merge_dat$Date >= as.Date("2019-12-01") & 
+                        Merge_dat$Date <= as.Date("2021-05-31"),
+                      !colnames(Merge_dat) %in% c("Date","D_Ann","D_Auc")], 2, sd))
