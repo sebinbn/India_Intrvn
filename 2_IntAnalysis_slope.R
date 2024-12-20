fdc482202a6ed69006655e622d41077013e491e3
unknown <sebinbn@gmail.com>
2024-12-20 12:16:21 -0600

    Intervention analysis completed. Pending is comparing average vs total impact and calculating total impact with methodology in paper.

--- 2_IntAnalysis_slope.R
+++ 2_IntAnalysis_slope.R
@@ -1,11 +1,37 @@
-# this file performs intervention analysis on slope. Returns the results in TF_101.
+# this file performs intervention analysis on slope. Returns the transfer function
+# in TF_101 and Int analysis in Int_101.
 # File follows 4-step process for first identifying transfer function model in 
 # pre-intervention data.
+
+# Note: Adding upto 2 lags of each covariate and then taking off insignificant 
+# variables doesn't work here as all covariates turn up insignificant. So, covariates
+# from CCF which show strange lags significant are used in TF and found to be significant.
+# Non-differenced Liq is added but is not significant, Liq_5 is significant both 
+# differenced and levels.
+# So, lags from CCF and Liq are used with Liq vars not differenced.
+
+
+# Creating data for different analysis ------------------------------------
 
 
 Period = cbind(
   Pre = Merge_dat$Date >= as.Date("2018-04-01") & Merge_dat$Date <= as.Date("2019-11-30"),
   Int = Merge_dat$Date >= as.Date("2019-12-01") & Merge_dat$Date <= as.Date("2021-05-31") )
+
+# Creating data for initial regression
+reg_dat = Merge_dat #subsetting smaller time period within lm makes 0 NAs when lagging
+reg_dat[c("WACR_12","Liq_5","EFFR_12","DGS10_5")] =
+  cbind(dplyr::lag(reg_dat$WACR,n = 12), dplyr::lag(reg_dat$Liq,n = 5),
+        dplyr::lag(reg_dat$EFFR,n = 12), dplyr::lag(reg_dat$DGS10,n = 5))
+
+# Creating first diff data
+diff_indx = !colnames(reg_dat) %in% c("Date", "Liq","Liq_3","Liq_5","D_Ann","D_Auc")
+diff_dat = reg_dat #creating placeholder
+diff_dat[-1,diff_indx] = apply(reg_dat[,diff_indx],2,diff)
+# subsetting for TF estimation
+tf_dat = diff_dat[Period[,"Pre"],]
+# subsetting for Int analysis estimation
+int_dat = diff_dat[Period[,"Int"],]
 
 
 # Identify pre-intervention transfer function -----------------------------
@@ -15,7 +41,7 @@ auto.arima(Merge_dat$s101[Period[,"Pre"]])#suggests 2,1,2
 #ACF and PACF checked to make own judgement regarding order
 acf(na.omit(diff(Merge_dat$s101[Period[,"Pre"]])))
 pacf(na.omit(diff(Merge_dat$s101[Period[,"Pre"]]))) #ACF and PACF at 1st lag significant
-mod_s101 = arima(Merge_dat$s101[Period[,"Pre"]], order = c(1,1,0)) #MA 1 slightly better than AR1. This is better than AR1. ARMA11 makes both insignificant.
+mod_s101 = arima(Merge_dat$s101[Period[,"Pre"]], order = c(0,1,1)) #1,1,0 > 0,1,1 in BIC. 1,1,1 has both insigniicant and of opposite signs.
 mod_s101
 BIC(mod_s101)
 acf(mod_s101$residuals);pacf(mod_s101$residuals) #ACF and PACF show residuals are white noise
@@ -37,37 +63,41 @@ par(mfrow =  op$mfrow, mai = op$mai) #reverting graphics options
 ## Step 2 - Linear regression with covariates -----------------------------------
 
 # lags identified from CCF are used in regression
-
-reg_dat = Merge_dat #subsetting smaller time period within lm makes 0 NAs when lagging
-reg_dat[c("WACR_12","Liq_5","EFFR_12","DGS10_5")] =
-  cbind(dplyr::lag(reg_dat$WACR,n = 12), dplyr::lag(reg_dat$Liq,n = 5),
-        dplyr::lag(reg_dat$EFFR,n = 12), dplyr::lag(reg_dat$DGS10,n = 5))
-reg101 = lm(s101 ~ WACR_12 + Liq +  EFFR_12 + DGS10_5, 
+reg101 = lm(s101 ~ WACR_12 + Liq  +  EFFR_12 + DGS10_5, 
             data = reg_dat[Period[,"Pre"],]) #adding Liq makes Liq_5 insignificant
 summary(reg101)
 
 ## Step 3 - ARIMA model on Linear regression errors-----------------------------
 acf(diff(reg101$residuals)); pacf(diff(reg101$residuals)) #suggests ARIMA(1,1,1)
-auto.arima(reg101$residuals)#suggest ARIMA 1,0,1 with zero mean
-# following checks if the residuals have unit root and finds null of unit root is rejected with p-value just at 0.05
+auto.arima(reg101$residuals)#suggest ARIMA 3,0,0 with zero mean
+# following checks if the residuals have unit root and finds null of unit root is rejected with 0.05 significance
 summary(ur.df(reg101$residuals, type = "trend",selectlags = "AIC"))
 summary(ur.df(na.omit(diff(reg101$residuals)),type = "trend",selectlags = "AIC"))
 
-ar101 = arima(reg101$residuals, order = c(0,1,1)) #this is not better than auto.arima
+ar101 = arima(reg101$residuals, order = c(1,1,0))
+# 1,1,1 > 0,1,1 > 1,1,0 > 2,1,0. But 1,1,1 has somewhat equal and opposite sign coefficients.
+# All better than auto.arima . AR(1) chosen as AR(1) and MA(1) coefficients similar magnitude.
 summary(ar101)
 BIC(ar101)
 
-
 ## Step 4 - Fitting Transfer function ------------------------------------------
 
+TF_101 = arima(tf_dat$s101, order = c(1,0,0),include.mean = F,
+               xreg = tf_dat[c("WACR_12","Liq","Liq_5","EFFR_12","DGS10_5")])
 
-reg_dat = reg_dat[Period[,"Pre"],]
-TF_101 = arima(reg_dat$s101, order = c(1,1,0), 
-               xreg = reg_dat[c("WACR_12","Liq","Liq_5","EFFR_12","DGS10_5")])
-                                                                  
 summary(TF_101) #The transfer function model identified
 (1-pnorm(abs(TF_101$coef)/sqrt(diag(TF_101$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+TF_101$nobs
+
+
+# Intervention Analysis ---------------------------------------------------
+
+Int_101 = arima(int_dat$s101, order = c(1,0,0),include.mean = F,
+               xreg = int_dat[c("WACR_12","Liq","Liq_5","EFFR_12","DGS10_5","D_Ann")])
+
+summary(Int_101) #The transfer function model identified
+(1-pnorm(abs(Int_101$coef)/sqrt(diag(Int_101$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+Int_101$nobs
 
 # Removing unnecessary variables ------------------------------------------
-
-rm(Period,mod_s101,fitwhite, fitwhite1,op, ar101)
+rm(Period,mod_s101,fitwhite, fitwhite1,op, ar101, reg_dat, diff_dat)
