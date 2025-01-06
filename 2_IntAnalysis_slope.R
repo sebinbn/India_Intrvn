0d24f682ac349f23fcd86fefc303d408458cb381
unknown <sebinbn@gmail.com>
2025-01-06 04:41:50 -0600

    Transfer function and Int analysis on slope completed after multiple xvars tried and best covariates chosen

--- 2_IntAnalysis_slope.R
+++ 2_IntAnalysis_slope.R
@@ -11,29 +11,6 @@
 # So, lags from CCF and Liq are used with Liq vars not differenced.
 
 
-# Creating data for different analysis ------------------------------------
-
-
-Period = cbind(
-  Pre = Merge_dat$Date >= as.Date("2018-04-01") & Merge_dat$Date <= as.Date("2019-11-30"),
-  Int = Merge_dat$Date >= as.Date("2019-12-01") & Merge_dat$Date <= as.Date("2021-05-31") )
-
-# Creating data for initial regression
-reg_dat = Merge_dat #subsetting smaller time period within lm makes 0 NAs when lagging
-reg_dat[c("WACR_12","Liq_5","EFFR_12","DGS10_5")] =
-  cbind(dplyr::lag(reg_dat$WACR,n = 12), dplyr::lag(reg_dat$Liq,n = 5),
-        dplyr::lag(reg_dat$EFFR,n = 12), dplyr::lag(reg_dat$DGS10,n = 5))
-
-# Creating first diff data
-diff_indx = !colnames(reg_dat) %in% c("Date", "Liq","Liq_3","Liq_5","D_Ann","D_Auc")
-diff_dat = reg_dat #creating placeholder
-diff_dat[-1,diff_indx] = apply(reg_dat[,diff_indx],2,diff)
-# subsetting for TF estimation
-tf_dat = diff_dat[Period[,"Pre"],]
-# subsetting for Int analysis estimation
-int_dat = diff_dat[Period[,"Int"],]
-
-
 # Identify pre-intervention transfer function -----------------------------
 
 ## Step 1: Identify ARIMA model -------------------------------------------
@@ -41,7 +18,7 @@ auto.arima(Merge_dat$s101[Period[,"Pre"]])#suggests 2,1,2
 #ACF and PACF checked to make own judgement regarding order
 acf(na.omit(diff(Merge_dat$s101[Period[,"Pre"]])))
 pacf(na.omit(diff(Merge_dat$s101[Period[,"Pre"]]))) #ACF and PACF at 1st lag significant
-mod_s101 = arima(Merge_dat$s101[Period[,"Pre"]], order = c(0,1,1)) #1,1,0 > 0,1,1 in BIC. 1,1,1 has both insigniicant and of opposite signs.
+mod_s101 = arima(Merge_dat$s101[Period[,"Pre"]], order = c(1,1,0)) #1,1,0 > 0,1,1 in BIC. 1,1,1 has both insigniicant and of opposite signs.
 mod_s101
 BIC(mod_s101)
 acf(mod_s101$residuals);pacf(mod_s101$residuals) #ACF and PACF show residuals are white noise
@@ -81,9 +58,13 @@ summary(ar101)
 BIC(ar101)
 
 ## Step 4 - Fitting Transfer function ------------------------------------------
+xvars = c("Liq","Liq_1", "Liq_2","WACR","WACR_1","WACR_2",
+          "EFFR","EFFR_1","EFFR_2", "DGS10","DGS10_1","DGS10_2")
+xvars = c("Liq","WACR","EFFR_1")
+xvars = c("WACR_12","Liq","Liq_5","EFFR_12","DGS10_5")
 
-TF_101 = arima(tf_dat$s101, order = c(1,0,0),include.mean = F,
-               xreg = tf_dat[c("WACR_12","Liq","Liq_5","EFFR_12","DGS10_5")])
+TF_101 = arima(diff_dat[Period[,"Pre"], "s101"], order = c(1,0,0),
+               include.mean = F, xreg = diff_dat[Period[,"Pre"], xvars])
 
 summary(TF_101) #The transfer function model identified
 (1-pnorm(abs(TF_101$coef)/sqrt(diag(TF_101$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
@@ -92,12 +73,20 @@ TF_101$nobs
 
 # Intervention Analysis ---------------------------------------------------
 
-Int_101 = arima(int_dat$s101, order = c(1,0,0),include.mean = F,
-               xreg = int_dat[c("WACR_12","Liq","Liq_5","EFFR_12","DGS10_5","D_Ann")])
+Int_101 = arima(diff_dat[Period[,"Int"], "s101"], order = c(1,0,0),
+                include.mean = F, xreg = diff_dat[Period[,"Int"], 
+                                                  c(xvars,"D_Ann")])
 
 summary(Int_101) #The transfer function model identified
 (1-pnorm(abs(Int_101$coef)/sqrt(diag(Int_101$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
 Int_101$nobs
 
+Int_101_Auc = arima(diff_dat[Period[,"Int"], "s101"], order = c(1,0,0),
+                    include.mean = F, xreg = diff_dat[Period[,"Int"], 
+                                                      c(xvars,"D_Auc")])
+summary(Int_101_Auc) #The transfer function model identified
+(1-pnorm(abs(Int_101_Auc$coef)/sqrt(diag(Int_101_Auc$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+Int_101_Auc$nobs
+
 # Removing unnecessary variables ------------------------------------------
-rm(Period,mod_s101,fitwhite, fitwhite1,op, ar101, reg_dat, diff_dat)
+rm(mod_s101,fitwhite, fitwhite1,op, ar101)
