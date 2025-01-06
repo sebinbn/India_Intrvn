a37557874712519cbdbf77853d98673e338d0c72
unknown <sebinbn@gmail.com>
2025-01-06 04:39:02 -0600

    Using diff_dat to use non-differenced liquidity as covariate

--- 2_IntAnalysis_1yr.R
+++ 2_IntAnalysis_1yr.R
@@ -1,5 +1,5 @@
 # this file performs intervention analysis on 1yr yield. Returns the results of 
-# transfer fn in TF_1 and intervention analysis in Int_101 and Int_101_Auc.
+# transfer fn in TF_1 and intervention analysis in Int_1y and Int_1y_Auc.
 # File follows 4-step process for first identifying transfer function model in 
 # pre-intervention data. Since this gives weird lags for covariates as significant,
 # 1 and 2 lags are tried for all covariates in transfer function. After trying both,
@@ -63,8 +63,8 @@ xvars = c("Liq","Liq_1", "WACR","WACR_1",
 xvars = c("Liq","Liq_5","WACR_3", "DGS10","DGS10_1") #weird lags from CCF
 xvars = c("Liq","Liq_5","DGS10","DGS10_1") 
 
-TF_1 = arima(reg_dat[Period[,"Pre"],"GIND1Y"], order = c(1,1,0), 
-              xreg = reg_dat[Period[,"Pre"],xvars])
+TF_1 = arima(diff_dat[Period[,"Pre"],"GIND1Y"], order = c(1,0,0),
+             include.mean = F, xreg = diff_dat[Period[,"Pre"],xvars])
 
 summary(TF_1) #The transfer function model identified
 BIC(TF_1)
@@ -73,20 +73,20 @@ BIC(TF_1)
 
 # Intervention Analysis ---------------------------------------------------
 
-Int_101 = arima(diff_dat[Period[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+Int_1y = arima(diff_dat[Period[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
                 xreg = diff_dat[Period[,"Int"], c(xvars,"D_Ann")])
 
-summary(Int_101) #The transfer function model identified
-(1-pnorm(abs(Int_101$coef)/sqrt(diag(Int_101$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
-Int_101$nobs
+summary(Int_1y) #The transfer function model identified
+(1-pnorm(abs(Int_1y$coef)/sqrt(diag(Int_1y$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+Int_1y$nobs
 
-Int_101_Auc = arima(diff_dat[Period[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+Int_1y_Auc = arima(diff_dat[Period[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
                     xreg = diff_dat[Period[,"Int"], c(xvars,"D_Auc")])
-summary(Int_101_Auc) #The transfer function model identified
-(1-pnorm(abs(Int_101_Auc$coef)/sqrt(diag(Int_101_Auc$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
-Int_101_Auc$nobs
+summary(Int_1y_Auc) #The transfer function model identified
+(1-pnorm(abs(Int_1y_Auc$coef)/sqrt(diag(Int_1y_Auc$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+Int_1y_Auc$nobs
 
 
 # Removing unnecessary variables ------------------------------------------
 
-rm(Period,mod_1y,fitwhite, fitwhite1,op, ar101)
+rm(mod_1y,fitwhite, fitwhite1,op, ar101)
