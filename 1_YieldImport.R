e7c5ea0949d5213b12b39d8e864899d89cfcde24
unknown <sebinbn@gmail.com>
2025-01-07 09:51:11 -0600

    Added OIS rates import.

--- 1_YieldImport.R
+++ 1_YieldImport.R
@@ -1,4 +1,4 @@
-# This imports data downloaded from Bloomberg. It then calculates slopes. 
+# This imports data on yields and OIS rates downloaded from Bloomberg.
 
 ####Data import and cleaning####
 
@@ -10,3 +10,14 @@ BBYield$Date = as.Date(BBYield$Date, format = "%Y-%m-%d")
 BBYield[,-1] = lapply(BBYield[,-1], as.numeric)
 
 BBYield = BBYield[order(BBYield$Date),]
+
+#Importing OIS rates
+OISYield = read_xlsx("Bloomberg data/IND_OIS_1mo1y.xlsx", range = 'A3:F1662')      # Data available for some variables upto 1998. But, importing data from 2018 Jan
+colnames(OISYield)[1] = "Date"
+colnames(OISYield)[-1] = c(substr(colnames(OISYield)[2],1,6),
+                          substr(colnames(OISYield)[3:6],1,5) )     #renaming with shorter names
+OISYield$Date = as.Date(OISYield$Date, format = "%Y-%m-%d") 
+OISYield[,-1] = lapply(OISYield[,-1], as.numeric)
+
+OISYield = OISYield[order(OISYield$Date),]
+
