a02dbcdc5def537bea03ec19344a51db2eb33b0f
Sebin Nidhiri <sebinbn@gmail.com>
2026-03-23 09:40:25 -0500

    Int Analysis with individual dummies run. p-values calculated from normal disn (not t disn), as MLE estimation gives asymptotically normal errors.

--- 2_IntAnalysis_1yr.R
+++ 2_IntAnalysis_1yr.R
@@ -1,5 +1,5 @@
 # this file performs intervention analysis on 1yr yield. Returns the results of 
-# transfer fn in TF_1 and intervention analysis in Int_1y and Int_1y_Auc.
+# transfer fn in TF_1 and intervention analysis in Int_1 and Int_1_Auc.
 # File follows 4-step process for first identifying transfer function model in 
 # pre-intervention data. Since this gives weird lags for covariates as significant,
 # 1 and 2 lags are tried for all covariates in transfer function. After trying both,
@@ -61,32 +61,72 @@ xvars = c("Liq","Liq_1", "Liq_2","WACR","WACR_1","WACR_2",
 xvars = c("Liq","Liq_1", "WACR","WACR_1",
           "EFFR","EFFR_1", "DGS10","DGS10_1")
 xvars = c("Liq","Liq_5","WACR_3", "DGS10","DGS10_1") #weird lags from CCF
-xvars = c("Liq","Liq_5","DGS10","DGS10_1") 
+xvars = c("Liq","DGS10") 
 
-TF_1 = arima(diff_dat[Period[,"Pre"],"GIND1Y"], order = c(1,0,0),
-             include.mean = F, xreg = diff_dat[Period[,"Pre"],xvars])
-
-summary(TF_1) #The transfer function model identified
+TF_1 = arima(diff_dat[Period_diff[,"Pre"],"GIND1Y"], order = c(1,0,0),
+             include.mean = F, xreg = diff_dat[Period_diff[,"Pre"],xvars])
+print("Transfer function model for 1 yr yield")
+print(summary(TF_1)) #The transfer function model identified
 BIC(TF_1)
-(1-pnorm(abs(TF_1$coef)/sqrt(diag(TF_1$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
-
+print("TF p-values")
+print((1-pnorm(abs(TF_1$coef)/sqrt(diag(TF_1$var.coef))))*2) #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+print(TF_1$nobs)
 
 # Intervention Analysis ---------------------------------------------------
 
-Int_1y = arima(diff_dat[Period[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
-                xreg = diff_dat[Period[,"Int"], c(xvars,"D_Ann")])
 
-summary(Int_1y) #The transfer function model identified
-(1-pnorm(abs(Int_1y$coef)/sqrt(diag(Int_1y$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
-Int_1y$nobs
+## For average impact -----------------------------------------------------------
 
-Int_1y_Auc = arima(diff_dat[Period[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
-                    xreg = diff_dat[Period[,"Int"], c(xvars,"D_Auc")])
-summary(Int_1y_Auc) #The transfer function model identified
-(1-pnorm(abs(Int_1y_Auc$coef)/sqrt(diag(Int_1y_Auc$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
-Int_1y_Auc$nobs
+print("Intervention analysis for 1 yr yield")
+Int_1 = arima(diff_dat[Period_diff[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+                xreg = diff_dat[Period_diff[,"Int"], c(xvars,"D_Ann")])
 
+print(summary(Int_1)) #The transfer function model identified
+print("1yr Int Analysis p-values")
+print((1-pnorm(abs(Int_1$coef)/sqrt(diag(Int_1$var.coef))))*2) #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+
+Int_1_Auc = arima(diff_dat[Period_diff[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+                    xreg = diff_dat[Period_diff[,"Int"], c(xvars,"D_Auc")])
+#Int_1_Auc = arimax(diff_dat[Period_diff[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+#                  xreg = diff_dat[Period_diff[,"Int"], xvars], 
+#                 xtransf = diff_dat[Period_diff[,"Int"],"D_Auc"], transfer = list(c(0,0)) ) #this gives same result as arima
+print(summary(Int_1_Auc)) #The transfer function model identified
+(1-pnorm(abs(Int_1_Auc$coef)/sqrt(diag(Int_1_Auc$var.coef))))*2 #calculating p-values. pnorm and not pt used as estimation is via MLE which gives asymptotically normal estimates. details here https://stats.stackexchange.com/questions/8868/how-to-calculate-the-p-value-of-parameters-for-arima-model-in-r
+
+
+## For individual dates --------------------------------------------------------
+
+# IntAnalysis_1yr = vector(mode = "list", length = nrow(Twist_Dates)*2)
+# names(IntAnalysis_1yr) = c(paste("Ann_", 1:24, sep = ""),
+#                            paste("Auc_", 1:24, sep = ""))
+# 
+# # Running one regression for each dummy variable
+# for (i in 1:nrow(Twist_Dates)){
+#   Dum_Ann = paste("D_Ann_",i, sep = "")
+#   Dum_Auc = paste("D_Auc_",i, sep = "")
+#   
+#   res_Ann = arima(diff_dat[Period_diff[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+#               xreg = diff_dat[Period_diff[,"Int"], c(xvars,Dum_Ann)])
+#   res_Auc = arima(diff_dat[Period_diff[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+#                   xreg = diff_dat[Period_diff[,"Int"], c(xvars,Dum_Auc)])
+#   
+#   Int_Ann_Indiv[i,7:9] = c(res_Ann$coef[Dum_Ann],
+#                            sqrt(diag(res_Ann$var.coef)[Dum_Ann]),
+#                            (1-pnorm(abs(res_Ann$coef)/sqrt(diag(res_Ann$var.coef))))[Dum_Ann]*2)
+#   Int_Auc_Indiv[i,7:9] = c(res_Auc$coef[Dum_Auc],
+#                            sqrt(diag(res_Auc$var.coef)[Dum_Auc]),
+#                            (1-pnorm(abs(res_Auc$coef)/sqrt(diag(res_Auc$var.coef))))[Dum_Auc]*2)
+#   
+#   IntAnalysis_1yr[paste("Ann_",i, sep = "")] = list(res_Ann) 
+#   IntAnalysis_1yr[paste("Auc_",i, sep = "")] = list(res_Auc)
+# }
+
+# Running a single regression with 24 dummies
+
+Int_1_Cum = arima(diff_dat[Period_diff[,"Int"],"GIND1Y"], order = c(1,0,0),include.mean = F,
+                  xreg = diff_dat[Period_diff[,"Int"], c(xvars,paste("D_Ann_",1:24,sep = ""))])
+sum(Int_1_Cum$coef[paste("D_Ann_",1:24,sep = "")])
 
 # Removing unnecessary variables ------------------------------------------
 
-rm(mod_1y,fitwhite, fitwhite1,op, ar101)
+rm(mod_1y,fitwhite, fitwhite1,op, ar1, Dum_Ann, Dum_Auc, res_Ann, res_Auc)
