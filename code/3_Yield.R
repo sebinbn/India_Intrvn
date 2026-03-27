309c5556990c3d597ba8eaf3e32e65dd9a4d8aa1
Sebin Nidhiri <sebinbn@gmail.com>
2026-03-27 15:29:46 -0500

    Created slope plot (unused in paper).

--- code/3_Yield.R
+++ code/3_Yield.R
@@ -10,7 +10,7 @@
 # Subset yield data for the analysis period.
 PlotDat = BBYield[c("Date", "GIND1Y", "GIND10Y")]
 PlotDat[c("GIND1Y", "GIND10Y")] = lapply(PlotDat[c("GIND1Y", "GIND10Y")],
-                                         na.approx, na.rm = F)
+                                         na.locf, na.rm = F)
 PlotDat = PlotDat[PlotDat$Date >= as.Date("2018-04-01") &
                     PlotDat$Date <= as.Date("2021-06-30"),] 
 
@@ -44,28 +44,16 @@ message(sprintf("yield_plot saved as %s",
 # 2. Create slope plot --------------------------------------------------------
 
 # Subset data for the analysis period.
-PlotDat <- merge(BBYield[c("Date", "GIND1Y")], OISYield[c("Date", "IRSW1")],
-                 by = "Date")
+PlotDat <- MergedDat[c("Date", "s101")]
 PlotDat <- PlotDat[PlotDat$Date >= as.Date("2018-04-01") &
                      PlotDat$Date <= as.Date("2021-06-30"),]
 
-# Reshape data for plotting two series with one legend.
-PlotDat_long <- rbind(
-  data.frame(Date = PlotDat$Date, Tenor = "1Yr", Yield = PlotDat$GIND1Y),
-  data.frame(Date = PlotDat$Date, Tenor = "OIS1Yr", Yield = PlotDat$IRSW1)
-)
-
-
-yield_OIS_plot <- ggplot(PlotDat_long, aes(x = Date, y = Yield, colour = Tenor)) +
+yield_OIS_plot <- ggplot(PlotDat, aes(x = Date, y = s101)) +
   geom_line(linewidth = 0.9) +
-  scale_colour_manual(values = c("1Yr" = "blue", "OIS1Yr" = "red")) +
-  labs(x = NULL, y = "Yield (%)", colour = "Series" ) +
+  labs(x = NULL, y = "Yield (%)") +
   theme_minimal() +
   theme(axis.text         = element_text(size = 15),
-        axis.title        = element_text(size = 17),
-        legend.position   = c(0.2, 0.2),
-        legend.title      = element_blank(),
-        legend.background = element_rect(linetype = "solid", colour = "black"))
+        axis.title        = element_text(size = 17))
 
 # 2. Create OIS and yield plot --------------------------------------------------------
 
