aa06d7e36b4e47b5128d7bfe29fe1da5ea7c0df6
Sebin Nidhiri <sebinbn@gmail.com>
2026-04-08 15:00:32 -0500

    Redid file name change and na.locf() for filling NA that was lost with merge.

--- code/1_MergeData.R
+++ code/1_MergeData.R
@@ -1,98 +1,86 @@
-# This file merges all data to create Merge_dat and creates dummies as columns in
-# merged data.It then creates reg_dat (with lagged variables added) and 
-# diff_dat (first differenced data) 
+# PURPOSE: 1. Merge yield, covariates and create slope and dummy variables.
+#          2. Create levels and first differenced data used later for analysis.
+#
+# INPUT: BBYield, OISYield, Liq_dat, WACR_dat, EFFR_dat, US10yr_dat
+# 
+# OUTPUT: MergedDat, MergedDat_diff
+#
 
-Merge_dat = BBYield[BBYield$Date >= as.Date("2018-02-01") & 
-                      BBYield$Date <= as.Date("2021-06-30"),
-                    c("Date","GIND10Y", "GIND1Y")] #susbsetting from Feb 2018 to avoid loss of data when taking lags in Intervention Analysis 
-Merge_dat = merge(Merge_dat,OISYield[c("Date","IRSW1")], by = "Date", all.x = T)
-Merge_dat = merge(Merge_dat,Liq_dat, by = "Date", all.x = T)
-Merge_dat = merge(Merge_dat,WACR_dat, by = "Date", all.x = T)
-Merge_dat = merge(Merge_dat,EFFR_dat, by = "Date", all.x = T)
-Merge_dat = merge(Merge_dat,US10yr_dat, by = "Date", all.x = T)
+# 1. Create merged data file -------------------------------------------------
 
-
-# Handling NAs ------------------------------------------------------------
-
-# Days when GIND10Y is missing is omitted.
-# Remaining EFFR,DGS10 and GIND1y missing is filled with prev non-NA.
-
-Merge_dat = Merge_dat[!is.na(Merge_dat$GIND10Y),]
-Merge_dat[c("EFFR", "DGS10", "GIND1Y")] = na.locf(Merge_dat[c("EFFR", "DGS10","GIND1Y")]) #these are the only three with missing values now
-#Merge_dat$GIND1Y = na.approx(Merge_dat$GIND1Y, na.rm = F)
-#note that GIND1y interpolation is giving weaker results
-colSums(is.na(Merge_dat))
-# # following was used to visualize where missing values are to decide how to handle missing values
-# ggplot(Merge_dat, aes(x=Date, y = GIND10Y))+
-#   geom_line()
-# miss_dates = data.frame(Date = Merge_dat$Date, Miss = is.na(Merge_dat$GIND1Y))
-# ggplot(miss_dates, aes(x = Date)) +
-#   geom_blank() +  # Start with a blank plot to set up the x-axis
-#   geom_vline(data = subset(miss_dates, Miss == TRUE),  # Filter for TRUE values in Miss
-#              aes(xintercept = as.numeric(Date)), color = "red", linetype = "dashed") +
-#   scale_x_date(date_breaks = "6 months", date_labels = "%Y-%m") +  # Adjust x-axis labels
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis text
-        
-
-# Calculating empirical slope ---------------------------------------------
-
-#Converting to bps all interest rates
-Merge_dat[!colnames(Merge_dat) %in% c("Date","Liq")] = 
-  Merge_dat[!colnames(Merge_dat) %in% c("Date","Liq")]*100
-Merge_dat$s101 = Merge_dat$GIND10Y - Merge_dat$GIND1Y
+#susbset longer timeframe to  avoid loss of data when taking lags in Intervention Analysis 
+MergedDat = BBYield[BBYield$Date >= as.Date("2018-02-01") &
+                      BBYield$Date <= as.Date("2021-12-31"),
+                    c("Date", "GIND10Y", "GIND1Y")]
+MergedDat = merge(MergedDat, OISYield[c("Date", "IRSW1")], by = "Date", all.x = T)
+MergedDat = merge(MergedDat, Liq_dat, by = "Date", all.x = T)
+MergedDat = merge(MergedDat, WACR_dat, by = "Date", all.x = T)
+MergedDat = merge(MergedDat, EFFR_dat, by = "Date", all.x = T)
+MergedDat = merge(MergedDat, US10yr_dat, by = "Date", all.x = T) 
 
 
-# Creating Dummies --------------------------------------------------------
+# 2. Handling NAs ------------------------------------------------------------
 
-Merge_dat[c("D_Ann","D_Auc")] = 0
-Merge_dat$D_Ann[Merge_dat$Date %in% Twist_Dates$Announcement] = 1
-Merge_dat$D_Auc[Merge_dat$Date %in% Twist_Dates$Auction] = 1
+# Understanding where the NAs are
+NA_counter = function(df, start_end){
+  df = df[df$Date >= start_end[1] & df$Date <= start_end[2] , ]
+  obs_count = nrow(df)
+  na_count = colSums(is.na(df)) 
+  tab_NA = data.frame(
+    Variable        = names(na_count),
+    NA_count        = na_count,
+    NA_pct          = sprintf("%.1f %%", na_count/obs_count*100),
+    row.names       = NULL
+  )
+  message(sprintf(
+    "In the daily data sample from %s to %s with %d observations, missing values by variable:",
+    start_end[1], start_end[2], obs_count
+  ))
+  print(tab_NA, row.names = FALSE)
+}
 
-# Merge_dat$D_Ann[Merge_dat$Date %in% Twist_Dates$Announcement] = 1/nrow(Twist_Dates)
-# Merge_dat$D_Auc[Merge_dat$Date %in% Twist_Dates$Auction] = 1/nrow(Twist_Dates)
-# Merge_dat$D_Auc[Merge_dat$Date == Twist_Dates$Auction[24] + 2] = 1/nrow(Twist_Dates)
+NA_counter(MergedDat, AnalysisPeriod[c("Pre_Start","Pre_End")])
+NA_counter(MergedDat, AnalysisPeriod[c("Int_Start","Int_End")])
+
+MergedDat[names(MergedDat) != "Date"] = lapply(
+  MergedDat[names(MergedDat) != "Date"], function(x) {
+    zoo::na.locf(x, na.rm = FALSE)
+  })
+message("Missing values in MergedDat filled in by na.locf")
+
+# 3. Creating new variables ---------------------------------------------
+
+# Converting all interest rates to bps
+MergedDat[!colnames(MergedDat) %in% c("Date", "Liq")] =
+  MergedDat[!colnames(MergedDat) %in% c("Date", "Liq")] * 100
+
+# Calculating slope
+MergedDat$s101 = MergedDat$GIND10Y - MergedDat$GIND1Y
+
+# Creating Dummies 
+MergedDat[c("D_Ann","D_Auc")] = 0
+MergedDat$D_Ann[MergedDat$Date %in% Twist_Dates$Announcement] = 1
+MergedDat$D_Auc[MergedDat$Date %in% Twist_Dates$Auction] = 1
 
 
 # Dummies for individual days
 for (i in 1:nrow(Twist_Dates)){
-  Date_Ann = Twist_Dates$Announcement[i]
-  Date_Auc = Twist_Dates$Auction[i]
-  Merge_dat[paste(c("D_Ann_","D_Auc_"),i,sep = "")] = 0
-  Merge_dat[Merge_dat$Date == Date_Ann, paste("D_Ann_",i,sep = "")] = 1
-  Merge_dat[Merge_dat$Date == Date_Auc, paste("D_Auc_",i,sep = "")] = 1
+  MergedDat[paste(c("D_Ann_","D_Auc_"),i,sep = "")] = 0
+  MergedDat[MergedDat$Date == Twist_Dates$Announcement[i],
+            paste("D_Ann_",i,sep = "")] = 1
+  MergedDat[MergedDat$Date == Twist_Dates$Auction[i],
+            paste("D_Auc_",i,sep = "")] = 1
 }
  
-# Creating other dataframes needed for analysis ---------------------------
+# 4. Creating first differenced data ---------------------------
 
-# Required lagged variables are created and stored in reg_dat. This dataframe is 
-# subsetted to run linear regression in pre-int period.
+MergedDat_diff = MergedDat
+diff_indx = !colnames(MergedDat_diff) %in%
+  c("Date", "Liq", "D_Ann", "D_Auc",
+    paste0("D_Ann_", 1:24), paste0("D_Auc_", 1:24))
+MergedDat_diff[-1, diff_indx] = apply(MergedDat_diff[, diff_indx], 2, diff)
+MergedDat_diff = MergedDat_diff[-1, ]
 
-reg_dat = Merge_dat # Note: subsetting smaller time period within lm makes no NAs when lagging. So not subsetting here
-reg_dat[c("WACR_1","Liq_1","EFFR_1","DGS10_1",
-          "WACR_2","Liq_2","EFFR_2","DGS10_2",
-          "WACR_3", "Liq_5", "DGS10_+1", "DGS10_5",
-          "WACR_12","EFFR_12")] = 
-  cbind(dplyr::lag(reg_dat$WACR,n = 1), dplyr::lag(reg_dat$Liq,n = 1),
-        dplyr::lag(reg_dat$EFFR,n = 1),dplyr::lag(reg_dat$DGS10,n = 1),
-        dplyr::lag(reg_dat$WACR,n = 2), dplyr::lag(reg_dat$Liq,n = 2),
-        dplyr::lag(reg_dat$EFFR,n = 2),dplyr::lag(reg_dat$DGS10,n = 2),
-        dplyr::lag(reg_dat$WACR,n = 3),dplyr::lag(reg_dat$Liq,n = 5),
-        dplyr::lead(reg_dat$DGS10,n = 1),dplyr::lag(reg_dat$DGS10,n = 5),
-        dplyr::lag(reg_dat$WACR,n = 12), dplyr::lag(reg_dat$EFFR,n = 12))
+# Removing Unnecessary variables -----------------------------------------
 
-# need differenced data to have some variables in levels in transfer function and
-# intervention analysis. Specifying differencing order in arima differences all
-# variables
-diff_dat = reg_dat
-diff_indx = !colnames(diff_dat) %in% 
-  c("Date","Liq","Liq_1","Liq_2","Liq_5","D_Ann","D_Auc",
-    paste("D_Ann_", 1:24, sep = ""),paste("D_Auc_", 1:24, sep = "")) #variables that are not to be first differenced
-diff_dat[-1,diff_indx] = apply(diff_dat[,diff_indx],2,diff)
-diff_dat = diff_dat[-1,]
-
-
-# Removing Unnecessary varaiables -----------------------------------------
-
-
-rm(Date_Ann, Date_Auc, diff_indx,i)
+rm(NA_counter, diff_indx,i)
--- code/2_ADFTest.R
+++ code/2_ADFTest.R
@@ -1,5 +1,14 @@
-# This file runs ADF tests for each variable in pre-intervention and intervention
-# periods.
+# PURPOSE: 1. Runs ADF test for each variable in pre-intervention and intervention
+#               periods
+#
+# INPUT: MergedDat, MergedDat_diff
+# 
+# OUTPUT: ADF_tab
+
+# Preliminaries  ---------------------------------------------------------
+
+# Specify the variables on which the ADF tests are run.
+Vars_ADF = c("GIND10Y","GIND1Y","s101","IRSW1", "WACR", "Liq","DGS10", "EFFR")
 
 ADF_tab = matrix(NaN, length(Vars_ADF), 4)
 colnames(ADF_tab) = paste(c(rep("Apr18_Nov19",2), rep("Dec19_Jun21",2)),
@@ -10,17 +19,22 @@ rownames(ADF_tab) = Vars_ADF
 # Running ADF test in a loop ----------------------------------------------
 
 for (t in 1:2){ #loop over Pre and Int periods
+  
+  lvl_subset = MergedDat[MergedDat$Date >= AnalysisPeriod[2*t-1] &
+                           MergedDat$Date <= AnalysisPeriod[2*t],]
+  diff_subset = MergedDat_diff[MergedDat_diff$Date >= AnalysisPeriod[2*t-1] &
+                                 MergedDat_diff$Date <= AnalysisPeriod[2*t],]
+  
   for (Var in Vars_ADF){ #loop over each variable
-    adf_res = summary(ur.df(Merge_dat[Period[,t],Var], type = "trend", 
-                            selectlags = "AIC") )
+    adf_res = summary(ur.df(lvl_subset[,Var], type = "trend", selectlags = "AIC"))
     ADF_tab[Var,2*t-1] = adf_res@testreg$coefficients["z.lag.1","t value"]
-    adf_res = summary(ur.df(na.omit(diff(Merge_dat[Period[,t],Var])),
-                            type = "trend", selectlags = "AIC") )
+    adf_res = summary(ur.df(diff_subset[,Var], type = "trend", selectlags = "AIC"))
     ADF_tab[Var,2*t] = adf_res@testreg$coefficients["z.lag.1","t value"]
   }
 }
 print("The crtical values of all tests are:")
 print(adf_res@cval)
+
 # Removing excess variables -----------------------------------------------
 
-rm(Vars_ADF, adf_res, t, Var)
+rm(Vars_ADF, lvl_subset, diff_subset,adf_res, t, Var)
