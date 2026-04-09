3e422dd4b439bd7274a56d6ed754a0a8b95fd760
Sebin Nidhiri <sebinbn@gmail.com>
2026-04-09 11:46:08 -0500

    updated code to show message after saving file, renamed variables in output to identify int and pre-int subset, redid changes for renamed MergedDat.

--- code/2_SummaryStats.R
+++ code/2_SummaryStats.R
@@ -1,4 +1,4 @@
-# This file uses Merge_dat created by 1_MergeData.R
+# This file uses Mergeddat created by 1_MergeData.R
 
 # Function to generate and tabulate summary statistics -------------------------
 
@@ -20,19 +20,31 @@ summ_stats <- function(df) {
   Stats_tab
 }
 
-# Calculate Summary statistics  -------------------------------
+# 1. Calculate Summary statistics for Pre and Int ------------------------------
 
-cols_to_summ = !colnames(Merge_dat) %in% c("Date","D_Ann","D_Auc",
+cols_to_summ = !colnames(MergedDat) %in% c("Date","D_Ann","D_Auc",
                                            paste0("D_Ann_", 1:24),
                                            paste0("D_Auc_", 1:24))
-# Pre-intervention summary -------------------------------------------------
 
-SummStats_tab = summ_stats(Merge_dat[Period[,"Pre"], cols_to_summ]) 
+Summ_pre = summ_stats(MergedDat[
+  MergedDat$Date >= AnalysisPeriod["Pre_Start"] &
+    MergedDat$Date <= AnalysisPeriod["Pre_End"],
+  cols_to_summ]) 
+Summ_pre$Variable = paste0(Summ_pre$Variable, "_Pre")
 
-# Intervention summary -------------------------------------------------
-SummStats_tab = rbind(SummStats_tab, 
-                      summ_stats(Merge_dat[Period[,"Int"], cols_to_summ])  )
+Summ_int = summ_stats(MergedDat[
+  MergedDat$Date >= AnalysisPeriod["Int_Start"] &
+    MergedDat$Date <= AnalysisPeriod["Int_End"],
+  cols_to_summ]) 
+Summ_int$Variable = paste0(Summ_int$Variable, "_Int")
 
-write.csv(SummStats_tab, file.path(OUTPUT, "SummaryStats.csv"), row.names = F)
+SummStats_tab = rbind(Summ_pre, Summ_int)
+                     
+# 2. Save Output & Remove temp variables --------------------------------------
 
-rm(summ_stats, cols_to_summ)
+filename_path = file.path(OUTPUT, "SummaryStats.csv")
+write.csv(SummStats_tab, filename_path , row.names = F)
+message(sprintf("Summary stats for both Pre-Int and Int periods calculaed, saved in %s",
+                paste(getwd(),filename_path,sep = "/") ))
+
+rm(summ_stats, cols_to_summ, Summ_pre, Summ_int, filename_path)
